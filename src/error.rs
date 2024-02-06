499fd2cac700679f93ddf3e1e2bb055e7e29e61d
bluesimp1102 <jackismyname7@gmail.com>
2024-02-06 13:53:29 -0800

    more errors handling

--- src/error.rs
+++ src/error.rs
@@ -3,36 +3,48 @@
 use std::error::Error;
 use std::fmt;
 
+pub type RustyResult<T> = Result<T, RustyError>;
+
 /// A custom error type for the application, covering various error scenarios encountered.
 ///
-/// This enum encapsulates different kinds of errors that can occur in the application, including
-/// network errors, JSON parsing errors, authentication errors, and other unexpected errors.
+/// This enum encapsulates different kinds of errors that can occur in the application, including specific handling for rate limiting by the Spotify API.
 #[derive(Debug)]
 pub enum RustyError {
     /// Represents errors that occur during network requests.
     Network(reqwest::Error),
     /// Represents errors that occur while parsing JSON data.
-    Parse(serde_json::Error),
+    ParseJson(serde_json::Error),
+    Io(std::io::Error),
     /// Represents errors related to token authentication failures.
     TokenAuthentication(String),
+    /// Represents being rate limited by the Spotify API and includes the duration to wait.
+    SpotifyRateLimited(u64), // Duration in seconds to wait before retrying
     /// Represents unexpected or miscellaneous errors.
     Unexpected(String),
+}
+
+impl RustyError {
+    pub fn invalid_input(msg: &str) -> Self {
+        RustyError::Io(std::io::Error::new(std::io::ErrorKind::InvalidInput, msg))
+    }
 }
 
 impl fmt::Display for RustyError {
     /// Provides a human-readable description of the error.
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            RustyError::Network(e) => write!(f, "network error: {}", e),
-            RustyError::Parse(e) => write!(f, "failed to parse data: {}", e),
-            RustyError::TokenAuthentication(msg) =>
-                write!(f, "token authentication error: {}", msg),
-            RustyError::Unexpected(msg) => write!(f, "an unexpected error occurred: {}", msg),
+            RustyError::Network(e) => write!(f, "network error: {e}"),
+            RustyError::ParseJson(e) => write!(f, "failed to parse data: {e}"),
+            RustyError::TokenAuthentication(msg) => write!(f, "token authentication error: {msg}"),
+            RustyError::SpotifyRateLimited(duration) =>
+                write!(f, "rate limited by Spotify API, retry after {duration} seconds"),
+            RustyError::Unexpected(msg) => write!(f, "an unexpected error occurred: {msg}"),
+            RustyError::Io(e) => write!(f, "input/output error: {e}"),
         }
     }
 }
 
-impl std::error::Error for RustyError {}
+impl Error for RustyError {}
 
 impl From<reqwest::Error> for RustyError {
     /// Converts `reqwest::Error` into `RustyError::Network`.
@@ -44,41 +56,12 @@ impl From<reqwest::Error> for RustyError {
 impl From<serde_json::Error> for RustyError {
     /// Converts `serde_json::Error` into `RustyError::Parse`.
     fn from(err: serde_json::Error) -> RustyError {
-        RustyError::Parse(err)
+        RustyError::ParseJson(err)
     }
 }
 
-/// Represents validation errors for seed data used in generating recommendations.
-///
-/// This struct encapsulates validation errors related to seed data inputs. It's used to signal
-/// when seed inputs for generating recommendations do not meet the required criteria.
-#[derive(Debug)]
-pub struct SeedValidationError {
-    /// Detailed message describing the validation error.
-    details: String,
-}
-
-impl SeedValidationError {
-    /// Creates a new `SeedValidationError` with a given message.
-    ///
-    /// # Arguments
-    ///
-    /// * `msg` - A message describing the validation error.
-    pub fn new(msg: &str) -> SeedValidationError {
-        SeedValidationError { details: msg.to_string() }
-    }
-}
-
-impl fmt::Display for SeedValidationError {
-    /// Provides a human-readable description of the validation error.
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.details)
-    }
-}
-
-impl Error for SeedValidationError {
-    /// Returns a description of the error.
-    fn description(&self) -> &str {
-        &self.details
+impl From<std::io::Error> for RustyError {
+    fn from(value: std::io::Error) -> Self {
+        RustyError::Io(value)
     }
 }
